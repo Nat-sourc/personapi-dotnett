3cf29ac6bf8c59b6ccb9447284d660262e89bf58
Natalia Gaona Salamanca <70587109+Nat-sourc@users.noreply.github.com>
2023-04-27 11:08:42 -0500

    Persona

--- personapi-dotnet/Program.cs
+++ personapi-dotnet/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using personapi_dotnet.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<SumaDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
